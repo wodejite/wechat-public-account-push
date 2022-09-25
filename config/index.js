6d118d1e7e89374f5c02184c2a5acedc0d2819fc
wodejite <114318111+wodejite@users.noreply.github.com>
2022-09-25 08:13:34 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -1,39 +1,35 @@
 export const config = {
     // 公众号配置
     // 公众号appId
-    appId: "wxc93158bacb37c180",
+    appId: "wx70c13e5f036afcdd",
     // 公众号appSecret
-    appSecret: "8166ccc94171205acc61d79daf86d08b",
+    appSecret: "ccc114d87ebc08483b421a469a463b20",
     // 模板消息id
-    templateId: "xvG5n3a5G8GETg4jKalnonM_jzTo2kOvJdXMgRdikwU",
+    templateId: "UOc1Js-jF93O1hf-Zc4WLICXgxhH6SQHDQyXqf1VTkY",
     // 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如["wx1", "wx2"]
-    user: ["oKMUE5wRy-yGT4pYhh3J2o-8F7rw","oKMUE5_d2TL5BYpWZiXM1dPLP6LA"],
+    user: ["g18831968036","yyy_yjyq"],
      
     // 信息配置
     ewai: "   ",
     // 所在省份
-    province: "广西",
+    province: "山西",
     // 所在城市
-    city: "桂林",
+    city: "阳泉",
     // 生日，修改名字为对应需要显示的名字，如果生日为农历， type为 r
     birthdays: [
-      {"name": "曾曾", "year": "1998", "date": "06-02", "type": 'new'},
+      {"name": "宝宝", "year": "2003", "date": "10-26", "type": 'new'},
       {"name": "哥哥", "year": "1996", "date": "01-07", "type": 'new'},
     ],
     // 在一起的日子，格式同上
-    loveDate: "2016-11-05",
-    // 结婚纪念日
+    loveDate: "2022-4-22",
     
-    }
 
- //{{date.DATA}}  
-//城市：{{city.DATA}}  
 //天气：{{weather.DATA}}  
 //最低气温: {{min_temperature.DATA}}  
 //最高气温: {{max_temperature.DATA}}  
-//今天是我们恋爱的第{{love_day.DATA}}天
+//今天是我们恋爱的第{{157.DATA}}天
 // 
  //{{birthday_message.DATA}}
 
-// {{note_en.DATA}}  
-// {{note_ch.DATA}}
+// {{note_en.DATA}}  Nothing in this world that's worth having comes easy.
+// {{note_ch.DATA}}这世界上凡是值得拥有的东西，都不易获得。
